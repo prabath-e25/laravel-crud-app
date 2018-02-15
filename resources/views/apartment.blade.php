a9fe892e89b8c873d116c88508dcf9772feb7554
prabath-e25 <prabath@eight25media.com>
2018-02-15 11:17:06 +0530

    update view

--- resources/views/apartment.blade.php
+++ resources/views/apartment.blade.php
@@ -1,6 +1,42 @@
 @extends('main')
   @section('content')
-  <h1>{{$apartment->apartment_name}}</h1>
-  <h1>{{$apartment->apartment_address}}</h1>
-  <h1>{{$apartment->no_of_rooms}}</h1>
+
+  <h1>Update Apartment</h1>
+  <div class="form-three widget-shadow">
+    <div class=" panel-body-inputin">
+      <form class="form-horizontal">
+        <div class="form-group">
+          <label class="col-md-3 control-label">Apartment Name</label>
+          <div class="col-md-9">
+            <div class="input-icon right spinner">
+              <input id="email" class="form-control1" value="{{$apartment->apartment_name}}" type="text" placeholder="Processing...">
+            </div>
+            <p class="help-block"></p>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label class="col-md-3 control-label">Apartment Address</label>
+          <div class="col-md-9">
+            <div class="input-icon right spinner">
+              <input id="email" class="form-control1" type="text" value="{{$apartment->apartment_address}}" placeholder="Processing...">
+            </div>
+            <p class="help-block"></p>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label class="col-md-3 control-label">no_of_rooms</label>
+          <div class="col-md-9">
+            <div class="input-icon right spinner">
+              <input id="email" class="form-control1" value="{{$apartment->no_of_rooms}}" type="text" placeholder="Processing...">
+            </div>
+            <p class="help-block"></p>
+          </div>
+        </div>
+
+      </form>
+    </div>
+  </div>
+
   @stop
--- resources/views/apartments.blade.php
+++ resources/views/apartments.blade.php
@@ -1,11 +1,33 @@
 @extends('main')
   @section('content')
+    <div class="tables">
     <h1>Apartments</h1>
     @if($apartments)
-    <ul>
-      @foreach ($apartments as $apartment)
-        <li><a href='./apartment/{{$apartment->id}}'>{{$apartment->apartment_name}}</a></li>
-      @endforeach
-    </ul>
+    <div class="bs-example widget-shadow" data-example-id="hoverable-table">
+      <h4>Hover Rows Table:</h4>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>no_of_rooms</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($apartments as $apartment)
+              <tr>
+                <th scope="row">{{$apartment->id}}</th>
+                <td>{{$apartment->apartment_name}}</td>
+                <td>{{$apartment->apartment_address}}</td>
+                <td>{{$apartment->no_of_rooms}}</td>
+                <td><a href='./apartment/{{$apartment->id}}'>edit</a></td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+    </div>
     @endif
+  </div>
   @stop
